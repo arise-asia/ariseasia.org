d9b6ae50f9f13e8cb5ad7970c988216c6c83856d
Jacky Lee <dr.jackylee@gmail.com>
2024-03-23 07:08:13 -0700

    Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"DM Sans"', "sans-serif"],
+      heading: ["Unbounded", "sans-serif"],
+    },
   },
   plugins: [],
 };
