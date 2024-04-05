509669a3fff08076a4ad9ad02f24f23868e892fe
Tamara Yustian <tamarayustian@gmail.com>
2024-04-06 03:19:52 +0800

    Add wavy lines in The Summary

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => (
         </div>
 
         <div className="relative bg-[#DB6645] rounded-xl w-4/5 mt-20">
-          <div className="absolute text-white text-3xl font-bold font-heading">
+          <div className="absolute text-white text-4xl font-bold font-heading">
             MISSION
           </div>
           <div className="p-20 py-8 text-center text-white text-xl italic">
@@ -38,7 +38,7 @@ const HomePage = () => (
 
         {/* UPCOMING MOVEMENTS */}
         <div className="relative bg-[#406E7D] rounded-xl w-2/5 p-10">
-          <div className="text-white text-3xl text-left font-bold font-heading leading-10">
+          <div className="text-white text-4xl text-left font-bold font-heading leading-10">
             UPCOMING
             <br />
             MOVEMENTS
@@ -160,44 +160,204 @@ const HomePage = () => (
 
         <div className="grid grid-rows-6 grid-flow-col gap-4 text-white">
           {/* 1st column */}
-          <div className="rounded-3xl bg-[#DB6645] row-span-3 flex flex-col justify-center items-center text-center">
-            <p className="font-heading font-bold text-3xl">37</p> countries
+          <div className="relative rounded-3xl bg-[#DB6645] row-span-3 flex flex-col justify-center items-center text-center aspect-w-1 aspect-h-1">
+            <p className="font-heading font-bold text-4xl">37</p> countries
             across Asia
+            {/* wave line 1 */}
+            <div className="absolute -top-1 -right-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="36"
+                height="37"
+                viewBox="0 0 36 37"
+                fill="none"
+              >
+                <path
+                  d="M1.72363 2C7.76098 3.03651 11.3659 8.34093 12.87 14.077C13.9579 18.2258 14.0193 25.1307 19.2376 26.1153C23.3208 26.8858 28.0359 26.8409 31.4716 29.4185C33.0109 30.5733 33.7304 31.937 34.1898 33.7941C34.5951 35.4325 34.6293 35.4646 33.9072 34.0463"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
+            {/* wave line 2 */}
+            <div className="absolute -bottom-1 -left-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="52"
+                height="38"
+                viewBox="0 0 52 38"
+                fill="none"
+              >
+                <path
+                  d="M2 2C9.7138 2 15.3181 7.85153 18.4071 14.6925C20.6413 19.6406 22.1887 28.2506 28.8749 28.3614C34.1067 28.4482 39.949 27.3812 44.7623 29.8635C46.9188 30.9757 48.1023 32.5245 49.0682 34.7453C49.9203 36.7044 49.9696 36.7372 48.7712 35.1208"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
           </div>
-          <div className="rounded-3xl bg-[#DB6645] row-span-3 text-center flex flex-col justify-center items-center text-center">
-            <p className="font-heading font-bold text-3xl">1844</p> Arise Asia
+
+          <div className="relative rounded-3xl bg-[#DB6645] row-span-3 text-center flex flex-col justify-center items-center text-center aspect-w-1 aspect-h-1">
+            <p className="font-heading font-bold text-4xl">1844</p> Arise Asia
             2023 attendees
+            {/* wave line */}
+            <div className="absolute -right-2 bottom-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="115"
+                viewBox="0 0 30 115"
+                fill="none"
+              >
+                <path
+                  d="M12.2273 112.849C-12.5607 92.0279 13.4728 67.9782 12.4748 42.5206C12.1418 34.0256 4.94818 23.3682 10.2467 15.1625C13.643 9.90274 21.4794 3.48589 27.6426 2"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
           </div>
 
           {/* 2nd column */}
-          <div className="rounded-3xl bg-[#A04C34] row-span-2 flex justify-center items-center p-3">
-            <p className="font-heading font-bold text-3xl mr-3">17</p> plenary
+          <div className="relative rounded-3xl bg-[#A04C34] row-span-2 flex justify-center items-center py-2 px-5">
+            <p className="font-heading font-bold text-4xl mr-3">17</p> plenary
             <br />
             speakers
+            {/* wave line */}
+            <div className="absolute -top-1 -right-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="120"
+                height="46"
+                viewBox="0 0 120 46"
+                fill="none"
+              >
+                <path
+                  d="M2 2.04187C7.97819 2.04187 13.9693 1.91964 19.9451 2.09285C30.2975 2.39292 37.9103 6.0461 46.9137 10.7085C57.8823 16.3887 70.7998 23.5645 83.2116 24.983C91.6545 25.9479 102.254 28.3837 108.651 34.3634C111.238 36.7819 113.857 38.6542 115.992 41.5006C118.863 45.3293 117.438 44.0678 114.87 41.5006"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
           </div>
-          <div className="rounded-3xl bg-[#A04C34] row-span-2 flex justify-center items-center p-3">
-            <p className="font-heading font-bold text-3xl mr-3">9</p> speakers
+
+          <div className="rounded-3xl bg-[#A04C34] row-span-2 flex justify-center items-center py-2 px-5">
+            <p className="font-heading font-bold text-4xl mr-3">9</p> speakers
           </div>
-          <div className="rounded-3xl bg-[#A04C34] row-span-2 flex justify-center items-center p-3">
-            <p className="font-heading font-bold text-3xl mr-3">60</p> workshop
+
+          <div className="relative rounded-3xl bg-[#A04C34] row-span-2 flex justify-center items-center py-2 px-5">
+            <p className="font-heading font-bold text-4xl mr-3">60</p> workshop
             <br />
             speakers
+            {/* wave line 1 */}
+            <div className="absolute -top-1 -left-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="76"
+                height="69"
+                viewBox="0 0 76 69"
+                fill="none"
+              >
+                <path
+                  d="M2.64875 66.703C1.70639 56.3358 1.43372 47.5536 9.189 39.4924C16.8954 31.4819 30.6855 29.4656 40.3954 24.5747C47.976 20.7562 51.8121 13.2296 58.5006 8.29952C62.8996 5.05699 68.6846 2.93779 74.108 2.44482"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
+            {/* wave line 2 */}
+            <div className="absolute -bottom-1 -right-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="51"
+                height="40"
+                viewBox="0 0 51 40"
+                fill="none"
+              >
+                <path
+                  d="M2 37.5581C2.64157 32.8532 6.79706 29.1433 10.3036 26.375C15.4338 22.3248 20.8244 18.2852 26.9777 15.9286C32.5062 13.8113 37.2661 11.3904 42.48 8.46206C45.979 6.49684 49.009 6.2165 49.009 2"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
           </div>
 
           {/* 3rd column */}
-          <div className="rounded-3xl bg-[#A34D4D] row-span-2 flex justify-center items-center p-3">
-            <p className="font-heading font-bold text-3xl mr-3">14</p> language
+          <div className="relative rounded-3xl bg-[#A34D4D] row-span-2 flex justify-center items-center py-2 px-5">
+            <p className="font-heading font-bold text-4xl mr-3">14</p>
+            language
+            <br />
             translations
+            {/* wave line */}
+            <div className="absolute -top-1 -right-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="81"
+                height="97"
+                viewBox="0 0 81 97"
+                fill="none"
+              >
+                <path
+                  d="M2.46642 2C1.90533 2 5.38819 2.86958 6.06384 3.24963C12.3133 6.76494 17.2588 12.6328 21.4002 18.3588C30.4467 30.8665 35.0466 45.9069 43.8935 58.5742C50.0997 67.4602 58.732 72.791 67.5229 78.7197C73.6698 82.8653 76.1988 88.8597 78.8074 95.3814"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
           </div>
-          <div className="rounded-3xl bg-[#A34D4D] row-span-2 flex justify-center items-center p-3">
-            <p className="font-heading font-bold text-3xl mr-3">40</p>{' '}
+
+          <div className="relative rounded-3xl bg-[#A34D4D] row-span-2 flex justify-center items-center py-2 px-5">
+            <p className="font-heading font-bold text-4xl mr-3">40</p>
             sponsoring &<br />
             endorsing partners
+            {/* wave line */}
+            <div className="absolute -bottom-2 -right-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="46"
+                height="84"
+                viewBox="0 0 46 84"
+                fill="none"
+              >
+                <path
+                  d="M43.8519 2C43.8519 8.43116 43.8592 14.5815 42.6623 20.9253C40.2198 33.8702 31.4262 44.1976 27.3599 56.3966C25.3238 62.5047 23.7199 68.8145 18.492 72.9968C14.7554 75.9861 6.98212 81.8105 2 81.8105"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
           </div>
-          <div className="rounded-3xl bg-[#A34D4D] row-span-2 flex justify-center items-center p-3">
-            <p className="font-heading font-bold text-3xl mr-3">90</p>{' '}
+
+          <div className="relative rounded-3xl bg-[#A34D4D] row-span-2 flex justify-center items-center py-2 px-5">
+            <p className="font-heading font-bold text-4xl mr-3">90</p>
             international &<br />
             local volunteers
+            {/* wave line */}
+            <div className="absolute -top-1 -left-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="56"
+                height="53"
+                viewBox="0 0 56 53"
+                fill="none"
+              >
+                <path
+                  d="M2 50.665C3.21653 50.3608 4.15418 46.7509 4.9199 45.5822C7.19897 42.1036 9.69214 38.884 12.2196 35.5788C16.3713 30.1497 20.3648 24.8553 24.6022 19.5194C28.312 14.8478 34.6847 11.0175 39.9587 8.38052C43.5341 6.59282 51.7038 5.76217 53.5849 2"
+                  stroke="#FCD369"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </div>
           </div>
 
           {/* 4th column */}
@@ -210,27 +370,27 @@ const HomePage = () => (
             />
           </div>
           <div className="rounded-3xl bg-[#9045DB] row-span-3 p-3">
-            <p className="font-heading font-bold text-3xl">499</p> Praying for 3
+            <p className="font-heading font-bold text-4xl">499</p> Praying for 3
             people to come to know Christ
           </div>
 
           {/* 5th column */}
           <div className="rounded-3xl bg-[#9045DB] row-span-3 p-3">
-            <p className="font-heading font-bold text-3xl">402</p> Pledged at
+            <p className="font-heading font-bold text-4xl">402</p> Pledged at
             least 1 year to a missions internship
           </div>
           <div className="rounded-3xl bg-[#9045DB] row-span-3 p-3">
-            <p className="font-heading font-bold text-3xl">229</p> Committed for
+            <p className="font-heading font-bold text-4xl">229</p> Committed for
             long-term cross-cultural mission
           </div>
 
           {/* 6th column */}
           <div className="rounded-3xl bg-[#9045DB] row-span-3 p-3">
-            <p className="font-heading font-bold text-3xl">462</p> To partner
+            <p className="font-heading font-bold text-4xl">462</p> To partner
             with 2 people for missions work
           </div>
           <div className="rounded-3xl bg-[#9045DB] row-span-3 p-3">
-            <p className="font-heading font-bold text-3xl">169</p> Applied for
+            <p className="font-heading font-bold text-4xl">169</p> Applied for
             1-2 years to do missions internship
           </div>
         </div>
