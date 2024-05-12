96f4c4a3b1986884b1b35d0cee51ab2faf052f96
Jacky Lee <dr.jackylee@gmail.com>
2024-05-12 00:10:23 -0700

    feat: change home page gif to mp4

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,11 +6,20 @@ import MomentsCarousel from "../components/MomentsCarousel";
 const HomePage = () => (
   <>
     {/* Header */}
-    <div className="px-10 pt-80 bg-center bg-no-repeat bg-cover bg-[url('../public/home.gif')]">
+    <div className="px-10 pt-80">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="object-cover absolute top-0 left-0 w-full h-full z-[-1]"
+      >
+        <source src="home.mp4" type="video/mp4" />
+      </video>
       <h1 className="text-6xl font-bold text-center text-orange-500 uppercase sm:text-7xl font-heading">
         Arise Asia
       </h1>
-      <p className="pt-2 pb-80 text-xl text-center text-white font-heading">
+      <p className="pt-2 pb-40 text-xl text-center text-white sm:pb-80 font-heading">
         Go to where there is no Gospel
       </p>
     </div>
