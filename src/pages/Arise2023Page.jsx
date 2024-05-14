9fbb2e194e2dbab3fd9f6edc976b451593fb781b
Tamara Yustian <tamarayustian@gmail.com>
2024-05-14 16:17:15 +0800

    Add desktop version for program

--- src/pages/Arise2023Page.jsx
+++ src/pages/Arise2023Page.jsx
@@ -1,15 +1,22 @@
-import day1Img from "../../public/schedule/day-1.jpg";
-import day2Img from "../../public/schedule/day-2.jpg";
-import day3Img from "../../public/schedule/day-3.jpg";
-import day4Img from "../../public/schedule/day-4.jpg";
-import day5Img from "../../public/schedule/day-5.jpg";
+import day1Img from '../../public/schedule/day-1.jpg';
+import day2Img from '../../public/schedule/day-2.jpg';
+import day3Img from '../../public/schedule/day-3.jpg';
+import day4Img from '../../public/schedule/day-4.jpg';
+import day5Img from '../../public/schedule/day-5.jpg';
 
 const Arise2023Page = () => (
   <div className="px-10 pt-60 bg-cyan-50">
     <h1 className="text-4xl font-bold text-center text-cyan-700 uppercase sm:text-5xl md:text-7xl font-heading">
       Arise 2023 Program
     </h1>
-    <div className="flex flex-col gap-8 items-center py-10 xl:flex-row justify-center">
+
+    {/* Desktop version */}
+    <div className="hidden md:block md:py-10">
+      <img alt="Program full" src="/program.png" />
+    </div>
+
+    {/* Mobile version */}
+    <div className="block md:hidden flex flex-col gap-8 items-center py-10 xl:flex-row justify-center">
       <img alt="Program day 1" src={day1Img} className="xl:w-1/6" />
       <img alt="Program day 2" src={day2Img} className="xl:w-1/6" />
       <img alt="Program day 3" src={day3Img} className="xl:w-1/6" />
