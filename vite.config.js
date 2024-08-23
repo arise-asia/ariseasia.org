abb2ba4a95d1a7fb9a0617c47dac1d8273cd7a4f
Jacky Lee <dr.jackylee@gmail.com>
2024-08-22 23:25:31 -0700

    perf: add image optimizer plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteYaml()],
+  plugins: [react(), ViteYaml(), ViteImageOptimizer()],
 });
