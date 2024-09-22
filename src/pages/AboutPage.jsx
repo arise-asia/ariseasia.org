b65e1ce5fc4e1256e8db4352204b7461756db6ca
Jacky Lee <dr.jackylee@gmail.com>
2024-09-21 23:11:17 -0700

    feat: add core members

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,6 @@
-import { MdOpenInNew } from "react-icons/md";
-
 const AboutPage = () => (
   <div className="flex flex-col pt-60 bg-red-50">
     <h1 className="text-center text-red-700 uppercase text-h1">About Us</h1>
-
-    <figure className="relative px-4 mx-auto mt-10 w-full max-w-screen-lg sm:mt-20">
-      <img
-        alt="Image of Arise Asia 2023"
-        className="object-cover rounded-xl aspect-video"
-        src="/about/about-us.jpg"
-      />
-      <figcaption className="pt-2 italic text-center">
-        Together for His mission: All 1,844 participants gathered at the closing
-        of Arise Asia 2023.
-      </figcaption>
-    </figure>
 
     <div className="container px-4 my-10">
       <h2 className="text-center text-red-700 uppercase translate-y-4 sm:translate-y-6 text-h2">
@@ -23,7 +9,7 @@ const AboutPage = () => (
       <div className="relative p-10 mx-auto bg-red-700 rounded-3xl w-fit">
         <img
           alt="What is Arise? logo"
-          className="hidden absolute bottom-2 right-8 w-12 sm:block xl:right-6 xl:-bottom-16 xl:w-24"
+          className="hidden absolute right-4 -bottom-10 w-14 md:block"
           src="/logo.svg"
         />
         <div className="text-white prose prose-sm sm:prose-base">
@@ -51,14 +37,6 @@ const AboutPage = () => (
             this generation!
           </p>
         </div>
-        <a
-          className="flex gap-x-2 items-center py-2 px-4 mt-6 text-sm font-bold bg-red-100 rounded-full border-4 border-black sm:text-base hover:bg-red-50 w-fit"
-          href="https://goarise.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn more at goarise.org <MdOpenInNew size={20} />
-        </a>
       </div>
     </div>
 
@@ -70,7 +48,7 @@ const AboutPage = () => (
       <path className="fill-current" d="M0 0H4V1Q3 3 2 1 1-1 0 1Z" />
     </svg>
 
-    <div className="py-10 px-4 bg-red-100">
+    <div className="flex flex-col gap-y-32 py-10 px-4 bg-red-100">
       <div className="container flex flex-col gap-y-10">
         <h2 className="text-center text-red-700 uppercase text-h2">
           Our Leaders
@@ -89,21 +67,13 @@ const AboutPage = () => (
             <p className="italic font-bold text-red-900 text-p">
               Regional Director for East Asia, Lausanne Movement
             </p>
-            <div className="relative py-8 px-10 mt-6 max-w-screen-sm bg-red-200 rounded-xl">
-              <p className="italic text-justify text-red-900 text-p">
-                My challenge to this generation: You have one life. The one
-                challenge, find a people &quot;where Christ is not known&quot;
-                and boldly go to &quot;where there is no gospel&quot; and lay
-                down your life for another people in the footsteps of our Lord
-                Jesus Christ.
-              </p>
-              <span className="absolute top-0 left-0 text-8xl font-bold text-red-700 translate-x-1/4 -translate-y-6 font-heading">
-                &ldquo;
-              </span>
-              <span className="absolute right-0 bottom-0 text-8xl font-bold text-red-700 -translate-x-1/4 translate-y-16 font-heading">
-                &rdquo;
-              </span>
-            </div>
+            <p className="mt-6 prose prose-sm sm:prose-base">
+              My challenge to this generation: You have one life. The one
+              challenge, find a people &quot;where Christ is not known&quot; and
+              boldly go to &quot;where there is no gospel&quot; and lay down
+              your life for another people in the footsteps of our Lord Jesus
+              Christ.
+            </p>
           </div>
         </div>
         <div className="flex flex-col gap-10 justify-center items-center lg:flex-row-reverse lg:items-end">
@@ -120,24 +90,212 @@ const AboutPage = () => (
             <p className="italic font-bold text-red-900 text-p">
               Lead Pastor, Harvest Mission Community Church
             </p>
-            <div className="relative py-8 px-10 mt-6 max-w-screen-sm bg-red-200 rounded-xl">
-              <p className="italic text-justify text-red-900 text-p">
-                Throughout history God has used different generations to fulfill
-                His purposes. We are on the precipice of God doing something
-                great with this generation. My hope and prayer is that we will
-                see a generation who is transformed by the Gospel and who will
-                then proclaim the Gospel to transform the world. May we
-                &quot;risk it all for the One who gave His all&quot; so that we
-                will see every nation, tribe, people, and language worshiping
-                the King of Kings and Lord of Lords.
-              </p>
-              <span className="absolute top-0 left-0 text-8xl font-bold text-red-700 translate-x-1/4 -translate-y-6 font-heading">
-                &ldquo;
-              </span>
-              <span className="absolute right-0 bottom-0 text-8xl font-bold text-red-700 -translate-x-1/4 translate-y-16 font-heading">
-                &rdquo;
-              </span>
+            <p className="mt-6 prose prose-sm sm:prose-base">
+              Throughout history God has used different generations to fulfill
+              His purposes. We are on the precipice of God doing something great
+              with this generation. My hope and prayer is that we will see a
+              generation who is transformed by the Gospel and who will then
+              proclaim the Gospel to transform the world. May we &quot;risk it
+              all for the One who gave His all&quot; so that we will see every
+              nation, tribe, people, and language worshiping the King of Kings
+              and Lord of Lords.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container flex flex-col gap-y-10 max-w-screen-lg">
+        <h2 className="text-center text-red-700 uppercase xl:text-left text-h2">
+          Arise Core Team
+        </h2>
+        <div className="flex flex-wrap gap-10 justify-center">
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Patrick Fung"
+                className="absolute rounded-full aspect-square"
+                src="/about/patrick.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
             </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Patrick
+              <br />
+              Fung
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Chair, Arise Board
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Philip Chang"
+                className="absolute rounded-full aspect-square size-full"
+                src="/about/philip.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Philip
+              <br />
+              Chang
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Board Member
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Jun Escobar"
+                className="absolute rounded-full aspect-square"
+                src="/about/jun.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Jun
+              <br />
+              Escobar
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Board Member
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              {/* <div className="grid absolute inset-0 z-20 grid-cols-3 grid-rows-3"> */}
+              {/*   {Array.from({ length: 9 }).map((_, index) => ( */}
+              {/*     <div */}
+              {/*       key={index} */}
+              {/*       className="border border-gray-400" */}
+              {/*     /> */}
+              {/*   ))} */}
+              {/* </div> */}
+
+              <img
+                alt="Jackie Ro"
+                className="absolute rounded-full aspect-square size-full"
+                src="/about/jackie.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Jackie
+              <br />
+              Ro
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Core Member
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Joanna Jing"
+                className="absolute rounded-full aspect-square"
+                src="/about/joanna.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Joanna
+              <br />
+              Jing
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Director of Events
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Peter Hsu"
+                className="absolute rounded-full aspect-square"
+                src="/about/peter.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Peter
+              <br />
+              Hsu
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Director of Mission Organizations Engagement
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Ashish Hirday"
+                className="absolute rounded-full aspect-square"
+                src="/about/ashish.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Ashish
+              <br />
+              Hirday
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Director of Church Engagement
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Brenda Olasiman"
+                className="absolute rounded-full aspect-square"
+                src="/about/brenda.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Brenda
+              <br />
+              Olasiman
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Executive Assistant
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Claire Balagot"
+                className="absolute rounded-full aspect-square"
+                src="/about/claire.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Claire
+              <br />
+              Balagot
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Administrative Associate
+            </p>
+          </div>
+          <div className="flex flex-col w-32 text-center sm:w-40">
+            <div className="relative">
+              <img
+                alt="Leinel Tiglao"
+                className="absolute rounded-full aspect-square"
+                src="/about/leinel.png"
+              />
+              <div className="m-2 bg-red-200 rounded-full size-28 sm:size-36" />
+            </div>
+            <h3 className="mt-4 text-red-700 uppercase text-h5">
+              Leinel
+              <br />
+              Tiglao
+            </h3>
+            <p className="font-bold leading-5 text-red-900 text-p">
+              Social Media Admin
+            </p>
           </div>
         </div>
       </div>
@@ -186,6 +344,18 @@ const AboutPage = () => (
         </div>
       </div>
     </div>
+
+    <figure className="relative px-4 mx-auto mb-10 w-full max-w-screen-lg">
+      <img
+        alt="Image of Arise Asia 2023"
+        className="object-cover rounded-xl aspect-video"
+        src="/about/about-us.jpg"
+      />
+      <figcaption className="pt-4 italic text-center">
+        Together for His mission: All 1,844 participants gathered at the closing
+        of Arise Asia 2023.
+      </figcaption>
+    </figure>
 
     <svg
       className="h-6 text-red-50 bg-red-100 sm:h-10"
